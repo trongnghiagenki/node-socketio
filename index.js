3c77317bd3b32ab22c196caa3b263cc8ee21850e
Nghia Nguyen <>
2023-03-21 14:10:17 +0700

    + demo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var WebSocketServer = require("ws").Server;
 var http = require("http");
 var express = require("express");
 var app = express();
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/"));
 
@@ -15,14 +15,12 @@ var wss = new WebSocketServer({ server: server });
 console.log("websocket server created");
 
 wss.on("connection", function (ws) {
-  var id = setInterval(function () {
-    ws.send(JSON.stringify(new Date()), function () {});
-  }, 1000);
-
   console.log("websocket connection open");
 
   ws.on("message", function (message) {
     console.log("received: %s", message);
+
+    ws.send("someone sent " + message);
   });
 
   ws.on("close", function () {
