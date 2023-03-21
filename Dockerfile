5d1f7872fe3a3d9b4ac6f3355010f21335587858
Nghia Nguyen <>
2023-03-21 13:40:41 +0700

    + change port

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,4 @@ RUN npm install
 # Define the entry point for the container
 CMD ["npm", "start"]
 
-EXPOSE 80
+EXPOSE 5000
--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-const webSocket = require("ws");
+var WebSocketServer = require("ws").Server;
+var http = require("http");
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5000;
 
-const server = new webSocket.Server({ port: "80" });
+app.use(express.static(__dirname + "/"));
 
-server.on("connection", (socket) => {
-  console.log("Client connected");
+var server = http.createServer(app);
+server.listen(port);
 
-  socket.on("message", (message) => {
-    socket.send(`We received your ${message}`);
+console.log("http server listening on %d", port);
+
+var wss = new WebSocketServer({ server: server });
+console.log("websocket server created");
+
+wss.on("connection", function (ws) {
+  var id = setInterval(function () {
+    ws.send(JSON.stringify(new Date()), function () {});
+  }, 1000);
+
+  console.log("websocket connection open");
+
+  ws.on("message", function (message) {
+    console.log("received: %s", message);
   });
 
-  socket.on("close", (event) => {
-    console.log("Client disconnected");
+  ws.on("close", function () {
+    console.log("websocket connection close");
+    clearInterval(id);
   });
 });
-
-console.log("socket initialized on port 8080");
